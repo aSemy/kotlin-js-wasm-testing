8032ff040d64e84b129a80ed057cc1a6d137d53e
Adam <897017+aSemy@users.noreply.github.com>
2024-04-01 15:13:16 +0200

    escape TC messages

--- src/jsHostedMain/kotlin/TestReport.kt
+++ src/jsHostedMain/kotlin/TestReport.kt
@@ -22,19 +22,22 @@ inline fun TestReport.asTest(name: String, test: () -> Unit) {
 
 fun TestReport.addSuiteStart(name: String): Int {
     val flowId = nextFlowId++
-    println("##teamcity[testSuiteStarted name='$name' flowId='$prefix$flowId']")
+    val flowIdStr = "${prefix}${flowId}".tcEscape()
+    println("##teamcity[testSuiteStarted name='${name.tcEscape()}' flowId='$flowIdStr']")
     return flowId
 }
 
 fun TestReport.addSuiteFinish(name: String, flowId: Int) {
-    println("##teamcity[testSuiteFinished name='$name' flowId='$prefix$flowId']")
+    val flowIdStr = "${prefix}${flowId}".tcEscape()
+    println("##teamcity[testSuiteFinished name='${name.tcEscape()}' flowId='$flowIdStr']")
 }
 
 fun TestReport.addTestStart(name: String): Int {
     if (reportTestsAsSuites) return addSuiteStart(name)
 
     val flowId = nextFlowId++
-    println("##teamcity[testStarted name='$name' captureStandardOutput='true' flowId='$prefix$flowId']")
+    val flowIdStr = "${prefix}${flowId}".tcEscape()
+    println("##teamcity[testStarted name='${name.tcEscape()}' captureStandardOutput='true' flowId='$flowIdStr']")
     return flowId
 }
 
@@ -43,8 +46,14 @@ fun TestReport.addTestFailure(name: String, exception: Throwable, flowId: Int) {
 
     val details = "(details)" // exception.stackTraceToString().replace('\n', ' ')
     println(
-        "##teamcity[testFailed name='$name' message='$exception'" +
-            " details='$details' flowId='$prefix$flowId']"
+        "##teamcity[testFailed name='${name.tcEscape()}' message='${exception.tcEscape()}' details='${details.tcEscape()}' flowId='${"$prefix$flowId".tcEscape()}']"
+    )
+}
+
+fun TestReport.testIgnored(name: String, message: String, flowId: Int) {
+    val details = "(details)" // exception.stackTraceToString().replace('\n', ' ')
+    println(
+        "##teamcity[testIgnored name='${name.tcEscape()}' message='${message.tcEscape()}' flowId='${flowId.tcEscape()}']"
     )
 }
 
@@ -58,3 +67,31 @@ fun TestReport.addTestFailureAndFinish(name: String, exception: Throwable, flowI
     addTestFailure(name, exception, flowId)
     addTestFinish(name, flowId)
 }
+
+private fun Any?.tcEscape(): String {
+    if (this == null) return ""
+
+    return toString()
+        .replace(Regex("\\x1B.*?m"), "") // ANSI colour codes
+        .replace("|", "||")
+        .replace("\n", "|n")
+        .replace("\r", "|r")
+        .replace("[", "|[")
+        .replace("]", "|]")
+        .replace("\u0085", "|x") // next line
+        .replace("\u2028", "|l") // line separator
+        .replace("\u2029", "|p") // paragraph separator
+        .replace("'", "|'")
+}
+
+//fun formatMessage(
+//    tcMessage: String,
+//    vararg args: Any
+//): String {
+//    val formattedArguments = mutableListOf<Any>()
+//    args.map { param ->
+//        formattedArguments.add(param.tcEscape())
+//    }
+//    formattedArguments.add(0, tcMessage)
+//    return format(*formattedArguments.toTypedArray())
+//}
